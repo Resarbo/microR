7651bb8ecb619fa3eab575b5ebd857e2b03bd57e
Resarbo <org_od234@hotmail.com>
2023-07-25 18:20:13 -0500

    proyecto eureka

--- platos-service/src/main/java/com/platos/service/PlatosServiceApplication.java
+++ platos-service/src/main/java/com/platos/service/PlatosServiceApplication.java
@@ -2,8 +2,10 @@ package com.platos.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PlatosServiceApplication {
 
 	public static void main(String[] args) {
